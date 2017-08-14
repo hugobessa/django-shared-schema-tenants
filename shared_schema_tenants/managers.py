d5c450c662e318b5a11406ea2e85494463f0d38c
Hugo Bessa <hugo@bessa.me>
2017-08-14 11:55:27 -0300

    adds possibility of using managers without having tenant in the middleware

--- shared_schema_tenants/managers.py
+++ shared_schema_tenants/managers.py
@@ -6,36 +6,47 @@ from shared_schema_tenants.exceptions import TenantNotFoundError
 
 class SingleTenantModelManager(Manager):
 
-    def get_queryset(self):
-        tenant = get_current_tenant()
-        if tenant:
+    def get_queryset(self, tenant=None):
+        if not tenant:
+            tenant = get_current_tenant()
+            if tenant:
+                return super().get_queryset().filter(tenant=tenant)
+            else:
+                raise TenantNotFoundError()
+        else:
             return super().get_queryset().filter(tenant=tenant)
-        else:
-            raise TenantNotFoundError()
 
-    def create(self, *args, **kwargs):
-        tenant = get_current_tenant()
-        if tenant:
-            kwargs['tenant'] = tenant
-            return super().create(*args, **kwargs)
+    def create(self, tenant=None, *args, **kwargs):
+        if not tenant:
+            tenant = get_current_tenant()
+            if tenant:
+                kwargs['tenant'] = tenant
+                return super().create(*args, **kwargs)
+            else:
+                raise TenantNotFoundError()
         else:
-            raise TenantNotFoundError()
-
+            return super().create(tenant=tenant, *args, **kwargs)
 
 class MultipleTenantModelManager(Manager):
 
-    def get_queryset(self):
-        tenant = get_current_tenant()
-        if tenant:
+    def get_queryset(self, tenant=None):
+        if not tenant:
+            tenant = get_current_tenant()
+            if tenant:
+                return super().get_queryset().filter(tenants=tenant)
+            else:
+                raise TenantNotFoundError()
+        else:
             return super().get_queryset().filter(tenants=tenant)
-        else:
-            raise TenantNotFoundError()
 
-    def create(self, *args, **kwargs):
-        tenant = get_current_tenant()
-        if tenant:
-            with transaction.atomic():
-                model_instance = super().create(*args, **kwargs)
-                model_instance.tenants.add(tenant)
+    def create(self, tenant=None, *args, **kwargs):
+        if not tenant:
+            tenant = get_current_tenant()
+            if tenant:
+                with transaction.atomic():
+                    model_instance = super().create(*args, **kwargs)
+                    model_instance.tenants.add(tenant)
+            else:
+                raise TenantNotFoundError()
         else:
-            raise TenantNotFoundError()
+            return super().create(tenants=tenant)
