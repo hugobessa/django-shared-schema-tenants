358af8d82790f9527059025a45c0632b86c22ee8
Hugo Bessa <hugo@bessa.me>
2017-08-15 23:49:40 -0300

    refactors tenant_settings helper

--- shared_schema_tenants/helpers/tenant_settings.py
+++ shared_schema_tenants/helpers/tenant_settings.py
@@ -1,14 +1,13 @@
 from shared_schema_tenants.settings import (
     DEFAULT_TENANT_SETTINGS_FIELDS, DEFAULT_TENANT_SETTINGS)
 
-from django.conf import settings
-
 from shared_schema_tenants.helpers.tenant_json_field import TenantJSONFieldHelper
 
 
 class TenantSettingsHelper(TenantJSONFieldHelper):
     TENANT_FIELDS = DEFAULT_TENANT_SETTINGS_FIELDS
     TENANT_DEFAULT_FIELDS_VALUES = DEFAULT_TENANT_SETTINGS
+
     def __init__(self, instance=None):
         super(TenantSettingsHelper, self).__init__(
             instance_field_name='settings', instance=instance)
