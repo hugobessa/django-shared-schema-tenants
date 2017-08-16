b9e7b57aee58a80740b291b05b99748de2dabc78
Hugo Bessa <hugo@bessa.me>
2017-08-15 23:49:55 -0300

    refactors createtenant management command

--- shared_schema_tenants/management/commands/createtenant.py
+++ shared_schema_tenants/management/commands/createtenant.py
@@ -1,12 +1,10 @@
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from shared_schema_tenants.models import Tenant
 from django.utils.text import slugify
 
+
 class Command(BaseCommand):
     help = 'Creates a new Tenant'
-
-    # def add_arguments(self, parser):
-    #     parser.add_argument('poll_id', nargs='+', type=int)
 
     def handle(self, *args, **options):
         name = input('Enter the Tenant name: ')
