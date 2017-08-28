1ada3849e5d16e49d947cbf8a8437ff2fdeda9d2
Hugo Bessa <hugo@bessa.me>
2017-08-28 02:09:16 -0300

    creates  tags endpoints

--- exampleproject/exampleapp/urls.py
+++ exampleproject/exampleapp/urls.py
@@ -15,8 +15,26 @@ urlpatterns = [
         name='list'
     ),
     url(
-        regex=r'^(?P<pk>[\w.@+-]+)/$',
+        regex=r'^(?P<pk>\d+)/$',
         view=views.ArticleViewSet.as_view({
+            'get': 'retrieve',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy',
+        }),
+        name='details'
+    ),
+    url(
+        regex=r'^tags/$',
+        view=views.TagViewSet.as_view({
+            'get': 'list',
+            'post': 'create',
+        }),
+        name='list'
+    ),
+    url(
+        regex=r'^tags/(?P<pk>\d+)/$',
+        view=views.TagViewSet.as_view({
             'get': 'retrieve',
             'put': 'update',
             'patch': 'partial_update',
