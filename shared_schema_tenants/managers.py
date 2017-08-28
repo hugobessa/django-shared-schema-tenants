de95bb3510190a78d30bd857487428e122f34060
Hugo Bessa <hugo@bessa.me>
2017-08-28 02:11:27 -0300

    fixes MultipleTenantModelManager create method

--- shared_schema_tenants/managers.py
+++ shared_schema_tenants/managers.py
@@ -1,4 +1,5 @@
 from django.db.models import Manager
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import transaction
 from shared_schema_tenants.helpers.tenants import get_current_tenant
 from shared_schema_tenants.exceptions import TenantNotFoundError
@@ -30,6 +31,9 @@ class SingleTenantModelManager(Manager):
 
 class MultipleTenantModelManager(Manager):
 
+    def _original_get_queryset(self):
+        return super(MultipleTenantModelManager, self).get_queryset()
+
     def get_queryset(self, tenant=None):
         if not tenant:
             tenant = get_current_tenant()
@@ -45,7 +49,10 @@ class MultipleTenantModelManager(Manager):
             tenant = get_current_tenant()
             if tenant:
                 with transaction.atomic():
-                    model_instance, created = super(MultipleTenantModelManager, self).get_or_create(*args, **kwargs)
+                    try:
+                        model_instance = self._original_get_queryset().get(**kwargs)
+                    except ObjectDoesNotExist:
+                        model_instance = super(MultipleTenantModelManager, self).create(*args, **kwargs)
                     model_instance.tenants.add(tenant)
                     return model_instance
             else:
