445bdeca2aff8d68f60fb6517163195943fede8c
Hugo Bessa <hugo@bessa.me>
2017-08-27 21:30:11 -0300

    fixes createtenant management command

--- shared_schema_tenants/management/commands/createtenant.py
+++ shared_schema_tenants/management/commands/createtenant.py
@@ -1,3 +1,5 @@
+from django.db import transaction
+from django.contrib.sites.models import Site
 from django.core.management.base import BaseCommand
 from shared_schema_tenants.models import Tenant
 from django.utils.text import slugify
@@ -9,10 +11,17 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         name = input('Enter the Tenant name: ')
         slug = input('Enter the Tenant slug: (%s)' % slugify(name))
+        domain = input('Enter the Tenant site: (localhost:8000)')
 
         if not slug:
             slug = slugify(name)
 
-        Tenant.objects.create(name=name, slug=slug)
+        if not domain:
+            domain = 'localhost:8000'
 
-        self.stdout.write(self.style.SUCCESS('Successfully created Tenant %s' % name))
+        with transaction.atomic():
+            tenant = Tenant.objects.create(name=name, slug=slug)
+            site = Site.objects.create(name=tenant.slug, domain=domain)
+            tenant.tenant_sites.create(site=site)
+
+            self.stdout.write(self.style.SUCCESS('Successfully created Tenant %s' % name))
